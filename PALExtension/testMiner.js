c97576c5e20c2d36af32cc8042ea0a1eec82e821
Wenley Tong <wenley.tong@gmail.com>
2012-04-02 11:40:38 -0400

    Successfully loads course content pages.

--- PALExtension/testMiner.js
+++ PALExtension/testMiner.js
@@ -1,9 +1,16 @@
+//  GLOBAL LINK VARIABLES
+var MasterCourseListClass = "portletList-img courseListing coursefakeclass";
+var CoursesTabLink = "https://blackboard.princeton.edu/webapps/portal/frameset.jsp?tab_tab_group_id=_2_1";
+var CoursesTabContentSrc = "/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_2_1";
+
+var parser = new DOMParser();
+
 
 
 //  Redirects the user to Courses tab
 function redirectToCourses() {
     console.log("Attempt redirect");
-    document.location.href = "https://blackboard.princeton.edu/webapps/portal/frameset.jsp?tab_tab_group_id=_2_1";
+    document.location.href = CoursesTabLink
 }
 
 
@@ -14,100 +21,100 @@ function mineSidebar() {
 
 
 //  Given a course page document, mines the course page
-function mineClass() {
+function mineClass(contentText) {
     console.log('In class');
+    
+    //  Show uniqueness of contentTexts
+    //  Can be verified
+    console.log(contentText.match(/var course_id = ".*";/g));
+}
+
+//  Extracts the content document text from a generic Blackboard page
+//  Requires String pageText and callback to continueFunc which will
+//  be called when the page finishes loading
+function getContentDoc(pageText, continueFunc) {
+   //  Get proper source link
+    var contentFrameTag = pageText.match(/<frame[^>]*name="content"[^>]*>/g)[0];
+    var contentFrameSrc = contentFrameTag.match(/src=\"[^ \"]*/g)[0].slice(5);
+    //  Slice chops off src="
+
+    //  Form link to desired page
+    var link = "https://blackboard.princeton.edu";
+    if (contentFrameSrc.substr(0, 5) == "https")
+       link = contentFrameSrc;
+    else
+       link += contentFrameSrc;
+
+    // Make request
+    var req = new XMLHttpRequest();
+    req.open("GET", contentFrameSrc, true);
+    req.onreadystatechange = function () {
+       if (req.readyState == 4 && req.status == 200) {
+          continueFunc(req.responseText);
+       }
+    }
+    req.send();
 }
 
 //  Takes an array of [Course Title, Course page link] arrays
 //  Does sanity check, then gets the texts of all course pages
 function writeArray(a) {
 
-   //  Simple sanity check from early stages
-    console.log(a[0][1]);
-    var req = new XMLHttpRequest();
-    var link = "https://blackboard.princeton.edu".concat(a[0][1]);
-    console.log(link);
-    req.open("GET", link, true);
-    req.onreadystatechange = function () {
-        console.log("Called anon");
-        if (req.readyState == 4 && req.status == 200) {
-            console.log("Get page");
-        }
-    }
-    req.send();
-
-
-    /* Failed attempt since the anonymous function doesn't know
-       who called it
-    var docs = new Array();
-    var reqs = new Array();
-    for (i = 0; i < a.length; i++) {
-       reqs[i] = new XMLHttpRequest();
-       link = "https://blackboard.princeton.edu" + a[i][1];
-       reqs[i].open("GET", link, true);
-       reqs[i].i = i;
-       reqs[i].onreadystatechange = function() {
-            if (req2.readState == 4 && req2.status == 200) {
-                console.log("Finished loading class page.");
-                console.log(req2.responseText.substr(0,40));
-            }
-        }
-        reqs[i].send();
-        } */
-    
-    //  Funky way of getting all the pages
-    function nextRequest() {
-       if (this.readyState == 4 && this.status == 200) {
-          //  Show part of text; Eventually replace with Get document
-          console.log(this.responseText.substr(150,40));
-
-          //  Check to see if need to do more
-          this.i = this.i + 1;
-          console.log(this.i);
-          if (this.i >= a.length) {
-             console.log("Stopping page loading process...");
-             return;
-          }
-
-          //  Get next page
-          //  Somehow has access to the variable a, even though only
-          //  called in the context of req2
-          link = "https://blackboard.princeton.edu" + a[this.i][1];
-          this.open("GET", link, true);
-          this.onreadstatechange = function () {
-             nextRequest.call(this);
-          }
-          this.send();
-       }
-    }
-
-    //  Attempt to get all pages sequentially
-    var req2 = new XMLHttpRequest();
-    req2.i = 0;
-    req2.docs = new Array();
-    link = "https://blackboard.princeton.edu" + a[req2.i][1];
-    req2.open("GET", link, true);
-
-    //  Start the chain reaction
-    req2.onreadystatechange = function () {
-       nextRequest.call(req2);
-    }
-    req2.send();
+   //  Funky way of getting all the pages
+   function nextRequest() {
+      if (this.readyState == 4 && this.status == 200) {
+         //  Show part of text; Eventually replace with Get document
+//         console.log(this.responseText.substr(150,40));
+         getContentDoc(this.responseText, mineClass);
+         
+         //  Check to see if need to do more
+         this.i = this.i + 1;
+//         console.log(this.i);
+         if (this.i >= a.length) {
+            console.log("Stopping page loading process...");
+            return;
+         }
+         
+         //  Get next page
+         //  Somehow has access to the variable a, even though only
+         //  called in the context of req2
+         link = "https://blackboard.princeton.edu" + a[this.i][1];
+         this.open("GET", link, true);
+         this.onreadstatechange = function () {
+            nextRequest.call(this);
+         }
+         this.send();
+      }
+   }
+   
+   //  Get all pages sequentially
+   var req2 = new XMLHttpRequest();
+   req2.i = 0;
+   req2.docs = new Array();
+   link = "https://blackboard.princeton.edu" + a[req2.i][1];
+   req2.open("GET", link, true);
+   
+   //  Start the chain reaction
+   req2.onreadystatechange = function () {
+      nextRequest.call(req2);
+   }
+   req2.send();
 }
 
-//  The big function. The function that mines EVERYTHING out of Blackboard
+//  The big function. The function that starts the mining of
+//  EVERYTHING out of Blackboard
 function mineBB() {
     console.log('In general miner');
     
     //  Check to make sure on Courses
     var contentFrame = document.getElementsByName("content")[0];
-    if (contentFrame.getAttribute("src") != "/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_2_1")
+    if (contentFrame.getAttribute("src") != CoursesTabContentSrc)
         redirectToCourses();
     
     //  Print Class Names and Links
     var contentDoc = contentFrame.contentDocument;
     var classListElem = undefined;
-    classListElem = contentDoc.getElementsByClassName("portletList-img courseListing coursefakeclass")[0];
+    classListElem = contentDoc.getElementsByClassName(MasterCourseListClass)[0];
     var classLinkList = classListElem.getElementsByTagName("a");
     
     var classesAndLinks = new Array();
