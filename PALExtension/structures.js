0e2bb930070826be3a276206b007006113acc488
Wenley Tong <wenley.tong@gmail.com>
2012-04-06 15:06:02 -0400

    Updated to have a structures.js file.

---
+++ PALExtension/structures.js
@@ -0,0 +1,33 @@
+function Instructor() {
+   this.name = "";
+   this.email = "";
+   this.office = "";
+   this.hours = "";
+   this.notes = "";
+}
+
+//  A folder in a sidebar elements
+function Folder() {
+   this.name = "";
+   this.link = "";
+}
+
+//  Container for information on an assignment
+function Assignment() {
+   this.name = "";
+   this.submissionLink = "";
+   this.fileLink = "";
+}
+
+//  Huge container for information on a particular Course
+function Course() {
+   this.sidebarElements = null;
+   this.announcements = null;
+   this.syllabusDoc = null;
+   this.descriptionLink = "";
+   this.courseMaterials = null;
+   this.assignments = null;
+   this.contacts = null;
+   this.toolLinks = null;
+   this.otherLinks = new Array();
+}
