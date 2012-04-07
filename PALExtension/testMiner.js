026d05b50b778deda6ecb4a4b572485503ec1f80
Maria Okounkova <mokounko@princeton.edu>
2012-04-06 21:40:42 -0400

    YOYO

--- PALExtension/testMiner.js
+++ PALExtension/testMiner.js
@@ -342,6 +342,8 @@ function mineMasha(link, course) {
     NameFinal = new Array();
     text4 = new Array();
     LinkFinal = new Array();
+    text6 = new Array();
+    TextFinal = new Array();
     
     req = new XMLHttpRequest();
     req.open("GET", link, true);
@@ -354,31 +356,21 @@ function mineMasha(link, course) {
 	    if (text2 != null) {
 		for (i = 0;i < text2.length; i++)
 		    {
-			if (text2 == null) {
-			    NameFinal[i] = "NOT A THING TYPE 1A";
-			    console.log("This course did not have assignments");
-			    break;}
-			else {
-			    txt = text2[i];
+   			    txt = text2[i];
 			    working = txt.match(/>[^<]*$/g)[0];
 			    if (working == null) {
 				NameFinal[i] = "NOT A THING TYPE 1B";
 				console.log("We really shouldn't be in here");
 				break;}
 			    else {
-				NameFinal[i] = txt.match(/>[^<]*$/g)[0].slice(1);}
+				NameFinal[i] = working.slice(1);}
 			}
 		    }
 		text4 = text.match(/((<div class="details" >[.|\s]*<table [^>]*>[.|\s]*(<tr>[.|\s]*)?<th[^<]*<\/th>[.|\s]*(<td>[.|\s]*)?<ul[^<]*(<li>[.|\s]*)?<a href[^<]*<img[^<]*>[^<]*<\/a>)|(<h3>[.|\s]*<span[^<]*<img[^<]*<\/span>[.|\s]*<a href[^>]*><span[^>]*>[^<]*))/g);
 		if (text4 != null) {
 		for (i = 0; i < text4.length ; i++)
 		    {
-			if (text4 == null) {
-			    LinkFinal[i] = "NOT A THING TYPE 2A";
-			    console.log("This course does not have assignment links");
-			    break;}
-			else {
-			    txt = text4[i];
+  			    txt = text4[i];
 			    working = txt.match(/((<a href[^<]*<img[^>]*>[^<]*<\/a>)|(<a href[^<]*>))/g)[0];
 			    if (working == null) {
 				LinkFinal[i] = "NOT A THING TYPE 2B";
@@ -388,8 +380,21 @@ function mineMasha(link, course) {
 				LinkFinal[i] = working;}
 			}
 		    }
-		}
+
+        text6 = text.match(/<div class="details" >[.|\s]*<div class="vtbegenerated">[^<]*/g);
+        if (text6 != null) {
+           for (i = 0; i < text6.length; i++)
+           {
+              txt = text6[i];
+              working = txt.match(/>[^<]*$/g)[0];
+              if (working == null)  {
+                 TextFinal[i] = "NOT A THING";
+                 break; }
+             else {
+                 TextFinal[i] = working.slice(1);}
+                                                     
 	    }
+        }
 	    
 	    if (text2 != null)
     	{
@@ -399,7 +404,8 @@ function mineMasha(link, course) {
     		var a = new Assignment();
     		a.name = NameFinal[i];
     		a.fileLink = LinkFinal[i];
-    		assignments[i] = a;
+    		a.submissionLink = TextFinal[i];
+            assignments[i] = a;
     		}
 	course.assignments = assignments;
     	}
