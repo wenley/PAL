c661342afadad9fcd1cbc570d40628f900d9ffd0
Maria Okounkova <mokounko@princeton.edu>
2012-04-10 03:38:18 -0400

    Added another thing

--- PALExtension/mineDocuments.js
+++ PALExtension/mineDocuments.js
@@ -1,90 +1,95 @@
 function mineMasha(link, course) {
-    console.log("I'M IN MINE MASHA!!!!!");
-    console.log(link);
-    console.log(course);
-    var txt;
-    var working;
-    text2 = new Array();
-    NameFinal = new Array();
-    text4 = new Array();
-    LinkFinal = new Array();
-    text6 = new Array();
-    TextFinal = new Array();
-    
-    req = new XMLHttpRequest();
-    req.open("GET", link, true);
+   //console.log("The mine masha link is \n" + link);
+   console.log("The mine masha course is \n" + course.title);
 
-    req.onreadystatechange = function () {
-	if (req.readyState == 4 && req.status == 200) {
-	      
-	    text = req.responseText;
-	    text2 = text.match(/<h3>[.|\s]*<span[^<]*<img[^<]*<\/span>[.|\s]*(<a href[^>]*>)?<span[^>]*>[^<]*/g);
-	    if (text2 != null) {
-           for (i = 0;i < text2.length; i++)
-           {
-              txt = text2[i];
-              working = txt.match(/>[^<]*$/g)[0];
-              if (working == null) {
-                 NameFinal[i] = "NOT A THING TYPE 1B";
-                 console.log("We really shouldn't be in here");
-                 break;
-              }
-              else {
-                 NameFinal[i] = working.slice(1);
-              }
-           }
-        }
-        text4 = text.match(/((<div class="details"[\s]*>[.|\s]*<table [^>]*>[.|\s]*(<tbody>[.|\s]*)?(<tr>[.|\s]*)?<th[^<]*<\/th>[.|\s]*(<td>[.|\s]*)?<ul[^<]*(<li>[.|\s]*)?<a href[^<]*<img[^<]*>[^<]*<\/a>)|(<h3>[.|\s]*<span[^<]*<img[^<]*<\/span>[.|\s]*<a href[^>]*><span[^>]*>[^<]*))/g);
-		if (text4 != null) {
-           for (i = 0; i < text4.length ; i++)
-           {
-              txt = text4[i];
-              working = txt.match(/((<a href[^<]*<img[^>]*>[^<]*<\/a>)|(<a href[^<]*>))/g)[0];
-              if (working == null) {
-                 LinkFinal[i] = "NOT A THING TYPE 2B";
-                 console.log("REALLY shouldn't be here");
-                 break;
-              }
-              else {
-                 LinkFinal[i] = working;
-              }
-           }
-        }
-        
-        text6 = text.match(/<div class="details" >[.|\s]*<div class="vtbegenerated">[^<]*/g);
-        if (text6 != null) {
-           for (i = 0; i < text6.length; i++)
-           {
-              txt = text6[i];
-              working = txt.match(/>[^<]*$/g)[0];
-              if (working == null)  {
-                 TextFinal[i] = "NOT A THING";
-                 break; }
-              else {
-                 TextFinal[i] = working.slice(1);}
-              
-           }
-        }
-	    
-	    if (text2 != null)
-    	{
-    	var assignments = new Array();
-    	for (i = 0; i < text2.length; i++)
-    		{
-    		var a = new Assignment();
-    		a.name = NameFinal[i];
-    		a.fileLink = LinkFinal[i];
-    		a.submissionLink = TextFinal[i];
-            assignments[i] = a;
-    		}
-	course.assignments = assignments;
-    	}
-    
-      	    console.log("THIS SHOULD BE THE ASSIGNMENT INFO BY MASHA");
-	    console.log(NameFinal);
-	    console.log(LinkFinal);
-	    console.log("THIS IS THE END OF ASSIGNMENT INFO BY MASHA");
-	}
-    }
-    req.send();
+   var req = new XMLHttpRequest();
+   req.open("GET", link, true);
+
+   req.onreadystatechange = function () {
+      if (req.readyState == 4 && req.status == 200) {
+
+         var NameTemp  = new Array();
+         var NameFinal = new Array();
+         var LinkTemp  = new Array();
+         var LinkFinal = new Array();
+         var MemoTemp  = new Array();
+         var MemoFinal = new Array();
+
+         var text;
+         var txt;
+         var texttemp;
+
+         text = req.responseText;
+
+         // Mine the names
+         NameTemp = text.match(/<h3>[.|\s]*<span[^<]*<img[^<]*<\/span>[.|\s]*(<a href[^>]*>)?<span[^>]*>[^<]*/g);
+         if (NameTemp != null) {
+            for (i = 0; i < NameTemp.length; i++)
+            {
+               txt = NameTemp[i];
+               texttemp = txt.match(/>[^<]*$/g)[0];
+               if (texttemp == null)
+               {
+                  console.log("Error, Masha's regular expression not matched");
+                  break;
+               }
+               else {
+                  NameFinal[i] = texttemp.slice(1);
+               }
+            }
+         }
+
+         // Mine the links
+         LinkTemp = text.match(/((<div class="details"[\s]*>[.|\s]*<table [^>]*>[.|\s]*(<tbody>[.|\s]*)?(<tr>[.|\s]*)?<th[^<]*<\/th>[.|\s]*(<td>[.|\s]*)?<ul[^<]*(<li>[.|\s]*)?<a href[^<]*<img[^<]*>[^<]*<\/a>)|(<h3>[.|\s]*<span[^<]*<img[^<]*<\/span>[.|\s]*<a href[^>]*><span[^>]*>[^<]*))/g);
+         if (LinkTemp != null) {
+            for (i = 0; i < LinkTemp.length; i++)
+            {
+               txt = LinkTemp[i];
+               texttemp = txt.match(/((<a href[^<]*<img[^>]*>[^<]*<\/a>)|(<a href[^<]*>))/g)[0];
+               if (texttemp == null)
+               {
+                  console.log("Error, Masha's regular expression not matched");
+                  break;
+               }
+               else {
+                  LinkFinal[i] = texttemp;
+               }
+            }
+         }
+
+         // Mine the memos
+         MemoTemp = text.match(/<div class="details" >[.|\s]*<div class="vtbegenerated">[^<]*/g);
+         if (MemoTemp != null) {
+            for (i = 0; i < MemoTemp.length; i++)
+            {
+               txt = MemoTemp[i];
+               texttemp = txt.match(/>[^<]*$/g)[0];    
+               if (texttemp == null)
+               {
+                  console.log("Error, Masha's regular expression not matched");
+                  break;
+               }
+               else {
+                  MemoFinal[i] = texttemp.slice(1);
+               }
+            }
+         }
+
+         // Store the assignments
+         if (NameTemp != null)
+         {
+            assignments = new Array();
+            for (i = 0; i < NameFinal.length; i++)
+            {
+               var a = new Assignment();
+               a.name = NameFinal[i];
+               a.fileLink = LinkFinal[i];
+               a.memo = MemoFinal[i];
+               assignments[i] = a;
+            }
+            course.assignments = assignments;
+         }
+      }
+   }
+   req.send();
 }
