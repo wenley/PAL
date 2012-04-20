023c297d3b76fb3b9809fdb2baa57bd85a955d04
Prerna <pramacha@princeton.edu>
2012-04-20 19:10:36 -0400

    initial version of populate, is it right?!

---
+++ PALExtension/populate.js
@@ -0,0 +1,12 @@
+// Author: Prerna Ramachandra
+// Written: 20 April 2012
+// populate.js
+
+// Populates template.html 
+function populate() {
+   for(var entry in courses) {
+      <li> <div> courses.semester <ul> </li>
+         if(course.semester.clicked == ture) {
+            expandSemester(courses.semester);
+            for(var entry in semester) {
+               <li> <div> course.name <ul> </li>
