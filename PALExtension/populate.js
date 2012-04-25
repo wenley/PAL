b873cb49271710b5e797179e0ec7090c98881e30
Prerna <pramacha@princeton.edu>
2012-04-25 10:06:26 -0400

    committing changes yay

--- PALExtension/populate.js
+++ PALExtension/populate.js
@@ -1,29 +1,22 @@
 // Author: Prerna Ramachandra
 // Written: 20 April 2012
 // newPopulate.js
-
-// Injects script for expanding semesters into original javascript page
-// Code adapted from stackoverflow.com, injecting javascript in a page
-function injectScript(scriptURL) {
-   var script = document.createElement('script');
-   script.setAttribute("type", "text/javascript");
-   script.setAttribute("src", scriptURL);
-   document.head.appendChild(script);
-}
-
+  
 // Populates template.html
 function populate() {
       var ul = document.body.getElementsByClassName("sideBarSemesters")[0];
       console.log(ul);
+
+      function foo() { expandSemester(this.sem); };
+
       for(var entry in Courses) {
          console.log(Courses[entry]);
          var li = document.createElement("li");
          var iLink = document.createElement("a");
 
-         iLink.setAttribute("href", "javascript:expandSemester('"+entry+"')");
          iLink.innerText = entry;
+         iLink.addEventListener("click", function() { expandSemester(this); }, false);
          li.appendChild(iLink);
          ul.appendChild(li);
-         injectScript(chrome.extension.getURL("expandSemesters.js"));
       }
 }
